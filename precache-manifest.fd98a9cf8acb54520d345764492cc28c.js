825df78bd0670a3a6fea097294d88bb51399ca0b
Adrian <eidiazcas@gmail.com>
2020-04-19 01:09:58 -0500

    Updates

---
+++ precache-manifest.fd98a9cf8acb54520d345764492cc28c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b05b2293108da2757b287c26e677c8",
+    "url": "/vchess/index.html"
+  },
+  {
+    "revision": "c7696ada802f37ae64cd",
+    "url": "/vchess/static/css/main.a23bea56.chunk.css"
+  },
+  {
+    "revision": "ca0db314707add377baf",
+    "url": "/vchess/static/js/2.c1c3c4ae.chunk.js"
+  },
+  {
+    "revision": "0b79ce434c8d9620c5369b07d77f19c3",
+    "url": "/vchess/static/js/2.c1c3c4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7696ada802f37ae64cd",
+    "url": "/vchess/static/js/main.42f33271.chunk.js"
+  },
+  {
+    "revision": "c286fdfcb0e5afc9c989",
+    "url": "/vchess/static/js/runtime-main.0c44992e.js"
+  },
+  {
+    "revision": "f24c419bb51f8ed3de16a27142bc8bd1",
+    "url": "/vchess/static/media/donation_request.f24c419b.wav"
+  },
+  {
+    "revision": "20e2bf1a2c41fba4a7b3253deed22ec8",
+    "url": "/vchess/static/media/ear.20e2bf1a.png"
+  },
+  {
+    "revision": "5acd1a089942ad6d903b600a76ef063c",
+    "url": "/vchess/static/media/failure.5acd1a08.mp3"
+  },
+  {
+    "revision": "5f759873017f7d2452b537de72984965",
+    "url": "/vchess/static/media/finally.5f759873.wav"
+  },
+  {
+    "revision": "f38f4ea39fd7d9bb93ca79ec61630b3f",
+    "url": "/vchess/static/media/heart.f38f4ea3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vchess/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90f18d922cb4ed167d094074361c9855",
+    "url": "/vchess/static/media/mic.90f18d92.svg"
+  },
+  {
+    "revision": "e0d006dd133f7750c0a9dbb7c2b0db17",
+    "url": "/vchess/static/media/success.e0d006dd.wav"
+  }
+]);
